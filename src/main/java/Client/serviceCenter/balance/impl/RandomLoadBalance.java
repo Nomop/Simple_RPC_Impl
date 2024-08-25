8c4df1c91e3503b7bd92ff813fea982baf56551c
Nomop <13098939400@163.com>
2024-08-25 18:42:58 +0800

    模拟实现随机负载均衡和轮询负载均衡

--- src/main/java/Client/serviceCenter/balance/impl/RandomLoadBalance.java
+++ src/main/java/Client/serviceCenter/balance/impl/RandomLoadBalance.java
@@ -3,9 +3,10 @@ package Client.serviceCenter.balance.impl;
 import Client.serviceCenter.balance.LoadBalance;
 
 import java.util.List;
+import java.util.Random;
 
 /**
- * @Description
+ * @Description 随机分配 负载均衡 模拟
  * @Author nomo
  * @Version 1.5
  * @Date 2024/8/25 18:32
@@ -14,6 +15,9 @@ public class RandomLoadBalance implements LoadBalance {
 
     @Override
     public String balance(List<String> addressList) {
+        Random random = new Random();
+        int choose = random.nextInt(addressList.size());
+        System.out.println("负载均衡选择" + choose + "服务器");
         return null;
     }
 
--- src/main/java/Client/serviceCenter/balance/impl/RoundLoadBalance.java
+++ src/main/java/Client/serviceCenter/balance/impl/RoundLoadBalance.java
@@ -5,15 +5,19 @@ import Client.serviceCenter.balance.LoadBalance;
 import java.util.List;
 
 /**
- * @Description
+ * @Description 轮询 负载均衡 模拟
  * @Author nomo
  * @Version 1.5
  * @Date 2024/8/25 18:33
  */
 public class RoundLoadBalance implements LoadBalance {
+    private int choose = -1;
     @Override
     public String balance(List<String> addressList) {
-        return null;
+        choose++;
+        choose = choose % addressList.size();
+        System.out.println("负载均衡选择" + choose + "服务器");
+        return addressList.get(choose);
     }
 
     @Override
